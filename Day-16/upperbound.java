3bf686880b92e593025c2949461f8e6093eb4deb
Narala Dayakar Reddy <158548132+dayakar-007@users.noreply.github.com>
2024-02-28 22:04:56 +0530

    Add files via upload

---
+++ Day-16/upperbound.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public static int upperBound(int []arr, int n, int x) {
+        int l=0;
+        int h=n-1;
+        int low=n;
+        while(l<=h)
+        {
+            int mid=(l+h)/2;
+            if(arr[mid]>x)
+            {
+                low=mid;
+                h=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+    }
+    return low;
+    }
+}
